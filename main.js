0ebbbcd3b5629e6c4e4882ca93fbfe02428cce8e
Graziela <grazims26@gmail.com>
2019-08-19 20:13:35 -0700

    fixed button styling and added button function

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
+const email = document.querySelector("#email");
 
-    const navLink = document.querySelectorAll('.nav ul a');
-    navLink.forEach(function(e) {
-        e.addEventListener('click', function(c) {
-            c.preventDefault();
-            document.querySelector(c.target.hash).scrollIntoView({behavior: "smooth"});
-        });
-    });
+const navLink = document.querySelectorAll(".nav ul a");
+navLink.forEach(function(e) {
+  e.addEventListener("click", function(c) {
+    c.preventDefault();
+    document
+      .querySelector(c.target.hash)
+      .scrollIntoView({ behavior: "smooth" });
+  });
+});
+
+document.querySelector(".form").addEventListener("submit", event => {
+  event.preventDefault();
+
+  if (email.value === "") {
+    alert("Please provide an email before subscribing!");
+  } else {
+    alert("Thank you for subscribing!");
+  }
+});
